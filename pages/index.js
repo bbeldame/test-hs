74381a6e943486e835161fc94030decd2a2e1d81
Basile Beldame <basile@hyperspread.com>
2021-10-20 14:53:57 +0200

    Piou piou

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Head from 'next/head'
+import { useQuery, gql } from "@apollo/client";
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+`;
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Test technique</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
+      <div className="container">
+        <Head>
+          <title>Test technique</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <Wrapper>
+          <div>COUCOU</div>
+        </Wrapper>
+      </div>
   )
 }
